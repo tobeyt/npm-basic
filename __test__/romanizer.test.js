9c539833d93fb49358170fb1e9226bf5ace52006
Tobey <bitsqiu@gmail.com>
2020-08-10 12:18:05 +0800

    [QD]: fixed: finish transform by romanize

---
+++ __test__/romanizer.test.js
@@ -0,0 +1,9 @@
+import { transform } from "../src/romanizer";
+
+describe("transform", () => {
+  it("should return X when input 10", () => {
+    const result = transform(10);
+
+    expect(result).toEqual("X");
+  });
+});
---
+++ src/romanizer.js
@@ -0,0 +1,3 @@
+import romanize from "romanize";
+
+export const transform = (input) => romanize(input);
